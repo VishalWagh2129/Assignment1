3e3b339468ddbc178dfb5ebde4fccb819e8002f8
Vishal Wagh <117093601+VishalWagh2129@users.noreply.github.com>
2024-06-07 13:41:59 +0530

    11 commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,14 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT
+  dialect: process.env.DB_DIALECT,
+  operatorAlias:false,
+    logging:false,
+    pool: {
+        max: 5,
+        idle: 30000,
+        acquire: 60000,
+    },
 });
 
 module.exports = sequelize;
